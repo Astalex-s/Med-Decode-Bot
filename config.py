98109491b692cf609a9480d8c17cc485fe981fa7
Astalex-t <astaf.al.mi@gmail.com>
2026-04-16 22:58:00 +0500

    add config.py and .env_example

---
+++ config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    BOT_TOKEN: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    REDIS_HOST: str
+    REDIS_PORT: int
+    OPENAI_API_KEY: str
+    YOOMONEY_TOKEN: str
+    FREE_LIMIT: int
+
+settings = Settings()
